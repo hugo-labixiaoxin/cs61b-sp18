26773abdc0d2ec482eac937e4964d2e9c8d1308a
Chenyu <1021313407@qq.com>
2020-11-07 17:23:00 +0800

    pro0 done!

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -39,6 +39,34 @@ public class NBody {
             a.draw();
         }
 
+        StdDraw.enableDoubleBuffering();
+
+        double time = 0;
+        while (time <= T) {
+            double[] xForces = new double[Planets.length];
+            double[] yForces = new double[Planets.length];
+            for (int i = 0; i<xForces.length; i+=1) {
+                xForces[i] = Planets[i].calcNetForceExertedByX(Planets);
+                yForces[i] = Planets[i].calcNetForceExertedByY(Planets);
+            }
+            for (int i = 0; i<Planets.length; i+=1) {
+                Planets[i].update(dt,xForces[i],yForces[i]);
+            }
+            StdDraw.picture(0,0,background);
+            for (Planet a : Planets) {
+                a.draw();
+            }
+            StdDraw.show();
+            StdDraw.pause(10);
+            time=time+dt;
+        }
+        StdOut.printf("%d\n",Planets.length);
+        StdOut.printf("%.2e\n", Radius);
+        for (int i = 0; i < Planets.length; i++) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                    Planets[i].xxPos, Planets[i].yyPos, Planets[i].xxVel,
+                    Planets[i].yyVel, Planets[i].mass, Planets[i].imgFileName);
+        }
     }
 }
 
